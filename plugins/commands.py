02721f1daccb17b907594a75963fe35e8ee7eb43
Akash Shibu <84174571+AKASH1234-bot@users.noreply.github.com>
2026-04-05 20:17:33 +0530

    Set protect_content to True for media sending

--- plugins/commands.py
+++ plugins/commands.py
@@ -197,7 +197,7 @@ async def start(client, message):
             msg = await client.send_cached_media(
                 chat_id=message.from_user.id,
                 file_id=file_id,
-                protect_content=True if pre == 'filep' else False,
+                protect_content=True,  # forwarding disabled
                 )
             filetype = msg.media
             file = getattr(msg, filetype.value)
@@ -240,7 +240,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        protect_content=True if pre == 'filep' else False,
+        protect_content=True,  # forwarding disabled
         reply_markup=file_buttons,
         )
                     
