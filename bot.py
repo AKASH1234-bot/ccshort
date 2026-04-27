792ca992dd3437a7410d9d047a51f9c4daa206e6
Akash Shibu <84174571+AKASH1234-bot@users.noreply.github.com>
2026-04-27 23:11:28 +0530

    Refactor bot.py to use dynamic PORT for web server

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import logging
 import logging.config
+
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
+
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
@@ -16,7 +18,9 @@ from pyrogram import types
 from aiohttp import web
 from plugins import web_server
 
+
 class Bot(Client):
+
     def __init__(self):
         super().__init__(
             name=SESSION,
@@ -39,14 +43,10 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
-
-        # Use port 8080 for bot web server to avoid conflict with gunicorn on PORT
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
-        bot_port = 8080
-        await web.TCPSite(app, bind_address, bot_port).start()
-
+        await web.TCPSite(app, bind_address, PORT).start()
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
 
@@ -65,10 +65,11 @@ class Bot(Client):
             new_diff = min(200, limit - current)
             if new_diff <= 0:
                 return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            messages = await self.get_messages(chat_id, list(range(current, current + new_diff + 1)))
             for message in messages:
                 yield message
                 current += 1
+
 
 app = Bot()
 app.run()
